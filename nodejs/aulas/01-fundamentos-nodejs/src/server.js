c45868b1684dd8d50b1d4fe0dba30b8ae6baf514
Dario Reis <guttyerresjunior1995@gmail.com>
2024-11-21 19:49:03 -0300

    feat(routes): add creation and listing routes to application structure
    - Implemented HTTP POST route for resource creation
    - Implemented HTTP GET route for resource listing
    - Added basic input validation for creation requests
    - Updated route definitions to follow RESTful conventions
    
    These changes establish a foundation for resource management within the application.

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,36 @@
 import http from 'node:http'
 
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HHTP
+//     - Método HTTP 
+//     - URL
+
+// GET, POST, PUT, DELETE, PATCH
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um novo recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+
+// GET /users => Buscando usuários do back-end
+// POST /users => Criando um novo usuário no back-end
+
 const server = http.createServer((req, res)=>{
+    const {url, method} = req
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Listagem de usuários')
+        }
+
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criando um novo usuário')
+        }
+
     return res.end('Hello World')
 })
 

0491b360efe0c14b3c3dc21d2643ef88c99573cd
Dario Reis <guttyerresjunior1995@gmail.com>
2024-11-24 20:43:08 -0300

    feat(node): support JSON body parsing with Streams and Buffers
    Added support for handling JSON request bodies using Streams and Buffers in Node.js. This implementation improves performance and memory usage for large payloads.
    
    - Implemented JSON parsing using Stream pipelines.
    - Handled edge cases for incomplete or malformed JSON data.
    - Added unit tests for the new functionality.

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -28,24 +28,40 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res)=>{
-    const {url, method} = req
+const server = http.createServer(async (req, res) => {
+    const { url, method } = req
 
-    if(method === 'GET' && url === '/users'){
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+
+    } catch {
+        req.body = null
+    }
+
+
+    if (method === 'GET' && url === '/users') {
         return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
-        }
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
 
-    if(method === 'POST' && url === '/users'){
+    if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
         users.push({
             id: 1,
-            name: 'João',
-            email: 'joao@gmail.com'
+            name,
+            email,
         })
         //return res.end('Criando um novo usuário')
         return res.writeHead(201).end()
-        }
+    }
 
     //return res.end('Hello World')
     res.writeHead(404).end()

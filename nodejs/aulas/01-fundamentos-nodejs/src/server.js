72bbf2de7a94e0e919c6757e0dbea3764aada0f0
Dario Reis <guttyerresjunior1995@gmail.com>
2024-11-26 20:56:32 -0300

    feat(routes): add support for route and query parameters
    Introduced functionality to handle both route and query parameters in the application.
    This enhancement allows more dynamic and flexible routing, improving URL structure and usability.
    
    Examples:
    - Route parameters: /users/:id
    - Query parameters: /users?id=123
    
    No breaking changes were introduced.

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -2,7 +2,18 @@ import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
-const users = []
+// Query parameters: URL stateful => Filtros, Paginação, Ordenação, Não-obrigatórios
+// Route Paramemeters: Identificação de recurso
+// Request Body: Envio de informações de um formulário (HTTPs)
+
+// http://localhost:3333/users?userId=1&name=Dario
+
+// GET http://localhost:3333/users/1
+// DELETE http://localhost:3333/users/1
+
+// POST http://localhost:3333/users
+
+// Edição e remoção 
 
 const server = http.createServer(async (req, res) => {
     const { url, method } = req

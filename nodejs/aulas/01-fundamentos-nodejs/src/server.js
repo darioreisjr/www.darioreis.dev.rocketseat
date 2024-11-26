427ab8c844323b1533e76119d0f7e3162479938f
Dario Reis <guttyerresjunior1995@gmail.com>
2024-11-26 20:36:58 -0300

    feat(routes): separate application routes into dedicated module
    Refactored the application to isolate route logic into a separate module, improving code organization and maintainability.

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,37 +1,6 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-
-// UUID => Unique Universal ID
-
-const database = new Database
-
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HHTP
-//     - Método HTTP 
-//     - URL
-
-// GET, POST, PUT, DELETE, PATCH
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um novo recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// DELETE => Deletar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-
-// GET /users => Buscando usuários do back-end
-// POST /users => Criando um novo usuário no back-end
-
-// Stateful - Stateless
-// JSON - JavaScript Object Notation
-// Cabeçalhos (Requisições / Respostas) => Metadados
-
-// HTTP Status Code
+import { routes } from './routes.js'
 
 const users = []
 
@@ -40,30 +9,15 @@ const server = http.createServer(async (req, res) => {
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users')
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-        return res
-            .end(JSON.stringify(users))
+    if (route) {
+        return route.handler(req, res)
     }
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        }
-
-        database.insert('users', user)
-
-        //return res.end('Criando um novo usuário')
-        return res.writeHead(201).end()
-    }
-
-    //return res.end('Hello World')
-    res.writeHead(404).end()
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
